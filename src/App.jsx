af160eae81930dea619e170b1bc203e65ca45a8a
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-05-17 14:59:10 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-center '>
+      <h1 className='text-5xl font-bold my-3'>Volunteer Network</h1>
+      <button className="btn btn-info">Button</button>
+    </div>
+  )
+}
+
+export default App
